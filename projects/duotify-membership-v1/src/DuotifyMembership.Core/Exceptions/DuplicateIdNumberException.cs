eccf175872c950d7daeb478095ed5110f194d44a
dtgfdgfgf <34129468+dtgfdgfgf@users.noreply.github.com>
2026-03-11 00:18:14 +0800

    Delete projects directory

--- projects/duotify-membership-v1/src/DuotifyMembership.Core/Exceptions/DuplicateIdNumberException.cs
+++
@@ -1,9 +0,0 @@
-namespace DuotifyMembership.Core.Exceptions;
-
-public class DuplicateIdNumberException : Exception
-{
-    public DuplicateIdNumberException(string idNumber) 
-        : base($"身分證字號 {idNumber} 已被註冊")
-    {
-    }
-}
--- projects/duotify-membership-v1/src/DuotifyMembership.Core/Interfaces/IVerificationService.cs
+++
@@ -1,7 +0,0 @@
-namespace DuotifyMembership.Core.Interfaces;
-
-public interface IVerificationService
-{
-    Task<bool> VerifyCodeAsync(Guid memberId, string code);
-    Task ResendCodeAsync(Guid memberId);
-}
